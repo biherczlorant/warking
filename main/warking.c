5e54c86ef772a132b07462636b797e0690123158
Lorant Bihercz <biherczlori@gmail.com>
2026-08-15 18:11:07 +0200

    main: testing gps and wifi

--- main/warking.c
+++ main/warking.c
@@ -1,6 +1,23 @@
+#include "esp_log.h"
+#include "gps_driver.h"
 #include "wifi.h"
 
 #include "nvs_flash.h"
+#include <freertos/FreeRTOS.h>
+
+static const char *TAG = "main";
+
+void main_task(void *arg) {
+  uint8_t buf[1024] = {0};
+  while (1) {
+    wifi_scan();
+    gps_driver_read(buf, 1024);
+
+    ESP_LOGI(TAG, "%s", buf);
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
 
 void app_main(void) {
   esp_err_t ret = nvs_flash_init();
@@ -10,6 +27,9 @@ void app_main(void) {
     ret = nvs_flash_init();
   }
   ESP_ERROR_CHECK(ret);
+  wifi_init();
+  gps_driver_init();
 
-  wifi_scan();
+  xTaskCreate(main_task, "main_task", configMINIMAL_STACK_SIZE * 4, NULL, 5,
+              NULL);
 }

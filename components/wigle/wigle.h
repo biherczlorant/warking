c960dde8810c23cb1c758903350170820632f5d1
Lorant Bihercz <biherczlori@gmail.com>
2026-08-22 16:20:31 +0200

    wigle: fully working wigle formatter

---
+++ components/wigle/wigle.h
@@ -0,0 +1,14 @@
+#ifndef __WIGLE_H__
+#define __WIGLE_H__
+
+#include <stdint.h>
+
+typedef struct {
+  char line[256];
+} wigle_row_t;
+
+int wigle_get_header(char *buf, size_t buf_len);
+int wigle_process(void);
+int wigle_get_total_unique(void);
+
+#endif // __WIGLE_H__

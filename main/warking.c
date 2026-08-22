564bd35b858fd71846e938826e259e2f7688546f
Lorant Bihercz <biherczlori@gmail.com>
2026-08-22 16:27:12 +0200

    main: adding lvgl, esp components, fully working app

--- main/warking.c
+++ main/warking.c
@@ -1,6 +1,10 @@
+#include "display.h"
 #include "esp_log.h"
 #include "gps.h"
+#include "sdcard.h"
+#include "ui.h"
 #include "wifi.h"
+#include "wigle.h"
 
 #include "nvs_flash.h"
 #include <freertos/FreeRTOS.h>
@@ -10,6 +14,16 @@ static const char *TAG = "main";
 void main_task(void *arg) {
   while (1) {
     wifi_scan();
+    wigle_process();
+
+    int total_aps = wigle_get_total_unique();
+    float distance_km = gps_get_distance_km();
+
+    if (display_lock(100)) {
+      lv_label_set_text_fmt(ui_Laps, "APs: %d", total_aps);
+      lv_label_set_text_fmt(ui_Ldist, "Dist: %.1f km", distance_km);
+      display_unlock();
+    }
     vTaskDelay(200 / portTICK_PERIOD_MS);
   }
 }
@@ -22,8 +36,19 @@ void app_main(void) {
     ret = nvs_flash_init();
   }
   ESP_ERROR_CHECK(ret);
+  char header[256];
+  wigle_get_header(header, sizeof(header));
+  ESP_ERROR_CHECK(sdcard_init(header, "wigle.txt"));
   wifi_init();
   gps_init();
+
+  lv_disp_t *disp = display_init();
+  if (disp) {
+    if (display_lock(portMAX_DELAY)) {
+      ui_init();
+      display_unlock();
+    }
+  }
 
   xTaskCreate(main_task, "main_task", configMINIMAL_STACK_SIZE * 4, NULL, 4,
               NULL);
